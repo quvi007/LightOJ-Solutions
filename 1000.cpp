00f590b9391ba9fcedf65eb655785dbe0d531e97
quvi007 <57809679+quvi007@users.noreply.github.com>
2019-12-09 20:21:53 +0600

    Create 1000.cpp

---
+++ 1000.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    for(int tc = 1; tc <= T; ++tc){
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << tc << ": " << (a + b) << '\n';
+    }
+    return 0;
+}
